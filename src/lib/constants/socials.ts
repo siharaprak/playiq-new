21395dd9ea99a79c79ef73fb9b7637b71c689cf3
Team Sienvi <teamsienvi@gmail.com>
2026-09-25 07:02:09 +0800

    fix(socials): update social media links for Facebook, TikTok, Instagram, and YouTube

---
+++ src/lib/constants/socials.ts
@@ -0,0 +1,6 @@
+export const PLAYIQ_SOCIAL_LINKS = {
+  facebook: 'https://www.facebook.com/people/We-Play-IQ/61583716970068/',
+  tiktok: 'https://www.tiktok.com/@weplayiq',
+  instagram: 'https://www.instagram.com/weplayiq',
+  youtube: 'https://www.youtube.com/channel/UCQzL5hQJHft_OjUT_SqsfMA',
+} as const;
